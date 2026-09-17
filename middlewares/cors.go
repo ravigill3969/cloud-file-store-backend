e191dc4ca46f4b164b700f110b57ed48bdbc6105
ravigill3936 <gill1234ravi@gmail.com>
2026-09-17 18:25:39 -0400

    cors fix

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -1,39 +1,27 @@
 package middleware
 
-import (
-	"net/http"
-)
+import "net/http"
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		origin := r.Header.Get("Origin")
 
-		allowedOrigins := map[string]bool{
-			"https://clouf-file-store-new-frontend.gill1122ravi.workers.dev": true,
-
-			// local development
-			"http://localhost:5173": true,
-			"http://localhost:3000": true,
-		}
-
-		if allowedOrigins[origin] {
+		if origin == "https://clouf-file-store-new-frontend.gill1122ravi.workers.dev" {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			w.Header().Set("Vary", "Origin")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set(
+				"Access-Control-Allow-Methods",
+				"GET, POST, PUT, PATCH, DELETE, OPTIONS",
+			)
+			w.Header().Set(
+				"Access-Control-Allow-Headers",
+				"Content-Type, Authorization",
+			)
 		}
 
-		w.Header().Set(
-			"Access-Control-Allow-Methods",
-			"GET, POST, PUT, PATCH, DELETE, OPTIONS",
-		)
-
-		w.Header().Set(
-			"Access-Control-Allow-Headers",
-			"Content-Type, Authorization, X-Requested-With",
-		)
-
-		// Browser preflight request
+		// Handle browser preflight request
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
 			return
