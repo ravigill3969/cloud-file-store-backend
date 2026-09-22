b1a6c3ecdc4fe5d7f41ec4747e8dcd260a5336f3
ravigill3936 <gill1234ravi@gmail.com>
2026-09-22 11:53:17 -0400

    added missing http method :: PATCH

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -15,7 +15,7 @@ func CORS(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set(
 			"Access-Control-Allow-Methods",
-			"GET, POST, PUT, DELETE, OPTIONS",
+			"GET, POST, PUT, DELETE, OPTIONS, PATCH",
 		)
 		w.Header().Set(
 			"Access-Control-Allow-Headers",
