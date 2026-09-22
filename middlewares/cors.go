9be52cc57efe70f624f53221bb67b40bdc5e956b
ravigill3936 <gill1234ravi@gmail.com>
2026-09-22 11:49:21 -0400

    fixed cors:: test in prod.

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -1,34 +1,26 @@
 package middleware
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
 
-		allowedOrigins := map[string]bool{
-			"https://cloud-stash.com":     true,
-			"https://www.cloud-stash.com": true,
-			"https://iloverher.com":       true,
-			"https://www.iloverher.com":   true,
-			"http://localhost:5173":       true,
-		}
+		log.Printf("CORS origin=%q", origin)
 
-		if allowedOrigins[origin] {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-		}
-
+		w.Header().Set("Access-Control-Allow-Origin", "https://cloud-stash.com")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set(
 			"Access-Control-Allow-Methods",
 			"GET, POST, PUT, DELETE, OPTIONS",
 		)
-
 		w.Header().Set(
 			"Access-Control-Allow-Headers",
 			"Content-Type, Authorization",
 		)
-
 		w.Header().Set("Vary", "Origin")
 
 		if r.Method == http.MethodOptions {
